1a8ab9681dd564f482a9fc46833e4958ed5f3e3a
Carlos Mendes <carls.mendes@gmail.com>
2017-08-18 10:34:14 +0100

    ajaxified

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,10 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
   end
+
+  # this action is going to render restaurants/destroy.js.erb
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
 end
